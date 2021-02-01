38d47366761d2a70ff0f1949b54ba336bf0a6b35
PompoenJeroen <48158580+PompoenJeroen@users.noreply.github.com>
2021-02-01 17:33:15 +0100

    Delete src/me/winterguardian/mobracers directory

--- src/me/winterguardian/mobracers/item/ItemRarity.java
+++
@@ -1,6 +0,0 @@
-package me.winterguardian.mobracers.item;
-
-public interface ItemRarity
-{
-	int getOdds(int position, int players);
-}
--- src/me/winterguardian/mobracers/state/MobRacersState.java
+++
@@ -1,9 +0,0 @@
-package me.winterguardian.mobracers.state;
-
-import me.winterguardian.core.game.state.State;
-import me.winterguardian.core.world.Region;
-
-public interface MobRacersState extends State
-{
-	Region getRegion();
-}
