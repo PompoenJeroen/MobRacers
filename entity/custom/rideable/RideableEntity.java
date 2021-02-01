6b9e035ba2dbda98c0713ed1dcce6e55460d1494
PompoenJeroen <48158580+PompoenJeroen@users.noreply.github.com>
2021-02-01 17:41:54 +0100

    Quality of Life

---
+++ entity/custom/rideable/RideableEntity.java
@@ -0,0 +1,25 @@
+package me.winterguardian.core.entity.custom.rideable;
+
+public interface RideableEntity
+{
+	float getClimbHeight();
+	void setClimbHeight(float climbHeight);
+
+	float getJumpHeight();
+	void setJumpHeight(float jumpHeight);
+	
+	float getJumpThrust();
+	void setJumpThrust(float jumpThrust);
+
+	float getSpeed();
+	void setSpeed(float speed);
+
+	float getAcceleration();
+	void setAcceleration(float acceleration);
+
+	float getBackwardSpeed();
+	void setBackwardSpeed(float backwardSpeed);
+
+	float getSidewaySpeed();
+	void setSidewaySpeed(float sidewaySpeed);
+}
